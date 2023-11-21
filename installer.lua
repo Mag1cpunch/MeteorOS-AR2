c5a6aee6cc2b619416b4a29c28a698a7c8bcee80
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-21 16:32:05 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,3 +1,14 @@
 if fs.exists("/MeteorOS") then
     fs.delete("/MeteorOS")
 end
+shell.run("wget https://raw.githubusercontent.com/Mag1cpunch/MeteorOS-AR2/main/startup.lua")
+shell.run("mkdir /MeteorOS")
+shell.run("cd /MeteorOS")
+shell.run("mkdir /MeteorOS/SystemX86")
+shell.run("mkdir /MeteorOS/SystemX86/CoreApps")
+shell.run("mkdir /MeteorOS/Programs")
+shell.run("cd /MeteorOS/SystemX86/CoreApps")
+shell.run("wget https://raw.githubusercontent.com/Mag1cpunch/MeteorOS-AR2/main/MeteorOS/SystemX86/CoreApps/shell.lua")
+shell.run("cd /MeteorOS/Programs")
+shell.run("wget https://raw.githubusercontent.com/Mag1cpunch/MeteorOS-AR2/main/MeteorOS/Programs/orescanner.lua")
+os.reboot()
