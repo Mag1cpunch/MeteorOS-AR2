2331fbe55a530c428027b5ba66381501bf43394d
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-22 12:14:13 +0200

    Update arterm.lua

--- MeteorOS/Programs/arterm.lua
+++ MeteorOS/Programs/arterm.lua
@@ -8,11 +8,4 @@ local function terminal()
     local canvas = modules.canvas()
     termcanvas = arterm.create(canvas, termsize / 2 + 40, termsize / 2 - 40, 30, 30, 2, true, false)
 end
-local function update()
-    termcanvas.redraw()
-end
 terminal()
-while true do
-    update()
-    sleep(0.1)
-end
