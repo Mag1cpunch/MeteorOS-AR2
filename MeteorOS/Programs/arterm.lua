cc16c263d38ecacfe21cbec48f5b858319e0c6c1
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-22 12:06:07 +0200

    Update arterm.lua

--- MeteorOS/Programs/arterm.lua
+++ MeteorOS/Programs/arterm.lua
@@ -1,7 +1,7 @@
 -- Add your module directory to the package path
 local moduleDirectory = "/MeteorOS/SystemX86/APIS/?.lua;" -- The question mark will be replaced by the module name
 package.path = package.path .. ";" .. moduleDirectory
-local arterm = require("ar_terminal.lua")
+local arterm = require("ar_terminal")
 local modules = peripheral.find("neuralInterface")
 local function terminal()
     local termsize = term.getSize()
