2c0cce29c8a9176cf909c6f7c2a4414f863ecb4a
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-22 12:10:58 +0200

    Update arterm.lua

--- MeteorOS/Programs/arterm.lua
+++ MeteorOS/Programs/arterm.lua
@@ -6,7 +6,7 @@ local modules = peripheral.find("neuralInterface")
 local function terminal()
     local termsize = term.getSize()
     local canvas = modules.canvas()
-    termcanvas = arterm.create(canvas, termsize / 2 + 40, termsize / 2 - 40, 300, 200, 2, true, false)
+    termcanvas = arterm.create(canvas, termsize / 2 + 40, termsize / 2 - 40, 30, 30, 2, true, false)
 end
 local function update()
     termcanvas.redraw()
